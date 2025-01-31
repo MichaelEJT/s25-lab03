540bac648d53d3085b64a35dedd26d765150bb1f
MichaelEJT <jingtong.e@gmail.com>
2025-01-30 22:15:36 -0500

    Implemented InheritanceSortedIntList, which used super to initiate the object with another private field: totalAdded. The Add function increments totalAdded by 1, and the addAll function calls its super.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,28 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+    private int totalAdded;
 
+    public InheritanceSortedIntList() {
+        super();
+        totalAdded = 0;
+    }
+
+
+    public boolean add(int value) {
+        totalAdded++;
+        return super.add(value);
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+
+    public boolean addAll(InheritanceSortedIntList collection) {
+
+        return super.addAll(collection);
+    }
 }
